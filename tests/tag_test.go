4a13e203ed17796ed0fc68d44ba4d691336b5832
Kidus Geremew <kidusg98@gmail.com>
2026-05-25 16:36:18 +0300

    feat: implement flexible attribute matching and enhance selector functionality

--- tests/tag_test.go
+++ tests/tag_test.go
@@ -2,9 +2,11 @@ package engine
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/halas77/goscrape/engine"
+	"golang.org/x/net/html"
 )
 
 func generateMockHTML(count int) string {
@@ -16,6 +18,130 @@ func generateMockHTML(count int) string {
 	html += `<span class="spam-category-item" id="footer-id">Footer is hear</span>`
 	html += "</body></html>"
 	return html
+}
+
+func FindMatchingAttributes(attrs []*engine.Attribute, elementAttrs []html.Attribute) bool {
+	lookup := make(map[string]string)
+	for _, attr := range attrs {
+		normalizedKey := strings.ToLower(attr.Key)
+		lookup[normalizedKey] = attr.Value
+	}
+
+	var attrsLength uint8 = uint8(len(attrs))
+	var counter uint8 = 0
+
+	for _, attr := range elementAttrs {
+		normalizedKey := strings.ToLower(attr.Key)
+		if valA, found := lookup[normalizedKey]; found {
+			if normalizedKey == "class" && engine.FlexSearch(valA, attr.Val, false) {
+				// if some one flexi Match to check the value and increment counter if it is true and continue
+				fmt.Println("---------- Hello ----------- ")
+				counter++
+				continue
+			}
+
+			if normalizedKey == "string" {
+				// use flex for equality and increment counter if it is true true and continue
+			}
+
+			if valA == attr.Val {
+				counter++
+			}
+		}
+	}
+
+	return attrsLength == counter
+}
+
+func nameSelector(n *html.Node, name string, attrs []*engine.Attribute) bool {
+	if n.Type == html.ElementNode && n.Data == name {
+		return FindMatchingAttributes(attrs, n.Attr)
+	}
+
+	return false
+}
+
+func traverse(n *html.Node, name string, attrs []*engine.Attribute) {
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		if nameSelector(c, name, attrs) {
+			// fmt.Println("Found Match ", c.Data)
+		}
+
+		traverse(c, name, attrs)
+	}
+}
+
+func TestFindMatchingAttributes(t *testing.T) {
+	tests := []struct {
+		name         string
+		attr         []*engine.Attribute
+		elementAttrs []html.Attribute
+		expected     bool
+	}{
+		{
+			name: "returns false when all attributes value does not match",
+			attr: []*engine.Attribute{
+				{Key: "id", Value: "submit-btn"},
+				{Key: "class", Value: "btn-primary"},
+			},
+			elementAttrs: []html.Attribute{
+				{Key: "class", Val: "btn-primary"},
+				{Key: "id", Val: "submit-button"},
+			},
+			expected: false,
+		},
+		{
+			name: "returns true when all attributes value match",
+			attr: []*engine.Attribute{
+				{Key: "id", Value: "submit-btn"},
+				{Key: "class", Value: "btn-primary"},
+			},
+			elementAttrs: []html.Attribute{
+				{Key: "class", Val: "btn btn-primary"},
+				{Key: "id", Val: "submit-btn"},
+			},
+			expected: true,
+		},
+	}
+
+	t.Run("Is Flexi work ", func(t *testing.T) {
+		result := engine.FlexSearch("btn btn-primary", "btn-primary", false)
+
+		if result != true {
+			t.Errorf("expected %v but found %v", true, result)
+		}
+	})
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			result := FindMatchingAttributes(tc.attr, tc.elementAttrs)
+			if result != tc.expected {
+				t.Errorf("expected %v but found %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+func BenchmarkMiniTest(b *testing.B) {
+	reader := strings.NewReader(generateMockHTML(50))
+	node, err := html.Parse(reader)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	b.StartTimer() // locate the position we will start the timer for the bench test
+	// 3. Run the actual loop
+
+	attr := []*engine.Attribute{
+		{Key: "id", Value: "footer-id"},
+	}
+
+	for b.Loop() {
+		traverse(node, "span", attr)
+	}
 }
 
 // BenchmarkFind benchmarks the Find method
